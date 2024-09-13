77d7cfd1e8a3bff39ab53da2643d548261e9f388
ItsAlbertZhang <itsalbertzhang@gmail.com>
2024-09-13 21:44:48 +0800

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+#if defined(_WIN32)
+    std::cout << "hello, Windows!" << std::endl;
+#elif defined(__APPLE__) && defined(__MACH__)
+    std::cout << "hello, MacOS!" << std::endl;
+#elif defined(__linux__)
+    std::cout << "hello, Linux!" << std::endl;
+#else
+    std::cout << "Unknown operating system" << std::endl;
+#endif
     return 0;
 }
