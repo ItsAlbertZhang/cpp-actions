e6556b82e127cf6a7d10a884c7952fdbec6760cc
ItsAlbertZhang <itsalbertzhang@gmail.com>
2024-09-14 11:41:41 +0800

    update main and yaml

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
 int main() {
-#if defined(_WIN32)
-    std::cout << "hello, Windows!" << std::endl;
-#elif defined(__APPLE__) && defined(__MACH__)
-    std::cout << "hello, MacOS!" << std::endl;
-#elif defined(__linux__)
-    std::cout << "hello, Linux!" << std::endl;
+#if defined(__clang__)
+    std::cout << "Clang version: " << __clang_major__ << "." << __clang_minor__
+              << "." << __clang_patchlevel__ << std::endl;
+#elif defined(__GNUC__) || defined(__GNUG__)
+    std::cout << "GCC version: " << __GNUC__ << "." << __GNUC_MINOR__ << "."
+              << __GNUC_PATCHLEVEL__ << std::endl;
+#elif defined(_MSC_VER)
+    std::cout << "MSVC version: " << _MSC_VER << std::endl;
 #else
-    std::cout << "Unknown operating system" << std::endl;
+    std::cout << "Unknown compiler" << std::endl;
 #endif
     return 0;
 }
